a9b8763bc142957fd67746bcf446b9ce3ef3a190
Evan Byrd <ebyrd@andrew.cmu.edu>
2017-12-12 22:37:08 -0500

    Temp must be above abs zero

--- temp_conversions.rb
+++ temp_conversions.rb
@@ -1,5 +1,6 @@
 def convert(temp)
 	return "Temperature must be an interger" unless temp.class == Fixnum
+	return "Temperature below Absolute Zero" if temp < -474
 	5 * (temp - 32)/9
 end
 
@@ -7,3 +8,4 @@ puts convert(32)
 puts convert(50)
 puts convert(212)
 puts convert("zero")
+puts convert(-500)
