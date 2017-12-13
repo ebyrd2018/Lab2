ae4fd3bdc9eb9649440448f9464e1182fb8f7631
Evan Byrd <ebyrd@andrew.cmu.edu>
2017-12-12 22:44:06 -0500

    Added measure param

--- temp_conversions.rb
+++ temp_conversions.rb
@@ -1,7 +1,12 @@
-def convert(temp)
+def convert(temp, measure="F")
 	return "Temperature must be an interger" unless temp.class == Fixnum
-	return "Temperature below Absolute Zero" if temp < -474
-	5 * (temp - 32)/9
+	if (measure == "F")
+		return "Temperature below Absolute Zero" if temp < -474
+		5 * (temp - 32)/9
+	else
+		return "Temperature below Absolute Zero" if temp < -270
+		((temp*9)/5)+32
+	end
 end
 
 puts convert(32)
@@ -9,3 +14,7 @@ puts convert(50)
 puts convert(212)
 puts convert("zero")
 puts convert(-500)
+puts convert(0, "C")
+puts convert(10, "C")
+puts convert(100, "C")
+puts convert(-280, "C")
